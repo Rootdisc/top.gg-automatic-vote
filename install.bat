d98839ae4f1e313f8a8e94bb3f78fc0fdceb9d84
Rootdisc <123176819+Rootdisc@users.noreply.github.com>
2023-01-24 20:29:49 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,11 @@
+echo off
+pip install pycryptodome
+cls
+pip install -r requirements.txt
+cls
+echo don't forget to put your tokens in file tokens.txt
+cls
+echo NOTE: Make sure you have Python installed. Otherwise this won't work
+echo https://www.python.org/downloads/
+echo wait for this to work. It takes a few minutes
+autovoter.py
